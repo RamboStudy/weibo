394c5afe5a15c98cb720543767711d29de3ce08b
Rambo <359466402@qq.com>
2020-12-10 08:03:37 +0000

    完成用户注册功能

---
+++ resources/views/shared/_messages.blade.php
@@ -0,0 +1,12 @@
+@foreach(['danger','warning','success','info'] as $msg)
+    @if(session()->has($msg))
+        <div class="flash-message">
+            <p class="alert alert-{{ $msg }}">
+               {{ session()->get($msg) }}
+            </p>
+        </div>
+    @endif
+@endforeach
+<!--
+session()->has($msg) 可用于判断会话中 $msg 键对应的值是否为空，若为空则在页面上不进行显示
+-->
